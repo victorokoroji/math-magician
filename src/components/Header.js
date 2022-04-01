93b3b9c15ed1fc9d742417e4905e334c411c4747
unknown <okorojiebube2@gmail.com>
2022-04-01 04:53:28 -0600

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => (
+	<header className='header'>
+		<h2>Math Magicians</h2>
+		<Navbar />
+	</header>
+)
+
+export default Header
